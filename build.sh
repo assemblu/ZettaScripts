2b611f479a21bbd0a383f8dd1a3034991ee43f11
assemblu <emir@assemb.lu>
2023-10-03 14:48:31 +0200

    fix navigation

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 cd "`dirname $0`"
-echo "$PWD"
-mkdir cyclonedds/build/
-cd cyclonedds/build/
-echo "$PWD"
+echo "Navigating to: $PWD"
+mkdir ../cyclonedds/build/
+cd ../cyclonedds/build/
+echo "Navigating to: $PWD"
 cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DBUILD_EXAMPLES=ON -DCMAKE_INSTALL_PREFIX=/Users/emir/dev/cyclonedds/install ..
+wait
 cmake --build . --parallel
 wait
 cmake --build . --target install
