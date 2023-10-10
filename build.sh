d22df1b4448b56ded9317a3f103a020610cfa251
assemblu <emir@assemb.lu>
2023-10-10 14:49:24 +0200

    linux-gnu recognition

--- build.sh
+++ build.sh
@@ -9,7 +9,11 @@ echo "Erasing previous build content"
 rm -rf *
 echo "Erasing previous install content"
 rm -rf ../install
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DBUILD_EXAMPLES=ON -DCMAKE_INSTALL_PREFIX=/Users/emir/dev/cyclonedds/install ..
+else
 cmake -G "Xcode" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DBUILD_EXAMPLES=ON -DCMAKE_INSTALL_PREFIX=/Users/emir/dev/cyclonedds/install ..
+fi
 wait
 cmake --build . --parallel
 wait
