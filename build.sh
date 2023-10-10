fa65f77988efd68955ca547fa0b335069f6123bc
assemblu <emir@assemb.lu>
2023-10-10 14:54:14 +0200

    plat

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+platform=$(uname)
+
 cd "`dirname $0`"
 echo "Navigating to: $PWD"
 mkdir ../cyclonedds/build/
@@ -9,11 +11,13 @@ echo "Erasing previous build content"
 rm -rf *
 echo "Erasing previous install content"
 rm -rf ../install
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+if [[ $platform == 'Linux' ]]; then
 cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DBUILD_EXAMPLES=ON -DCMAKE_INSTALL_PREFIX=/Users/emir/dev/cyclonedds/install ..
 else
 cmake -G "Xcode" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DBUILD_EXAMPLES=ON -DCMAKE_INSTALL_PREFIX=/Users/emir/dev/cyclonedds/install ..
 fi
+
 wait
 cmake --build . --parallel
 wait
