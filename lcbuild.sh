5217e693657f775894f4fbc4e2596afe5e45dfd2
assemblu <emirgoppa@gmail.com>
2023-11-07 16:33:41 +0100

    certified update

--- lcbuild.sh
+++ lcbuild.sh
@@ -11,7 +11,8 @@ echo "Erasing previous build content"
 rm -rf *
 echo "Erasing previous install content"
 rm -rf ../install
-cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DBUILD_EXAMPLES=ON -DCMAKE_INSTALL_PREFIX=/Users/emir/dev/cyclonedds-certified/install ..
+cd ../
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/Users/emir/dev/cyclonedds-certified/install -DSANITIZER=none -DENABLE_SSL=off -DENABLE_SECURITY=off -DENABLE_LIFESPAN=off -DENABLE_DEADLINE_MISSED=on -DENABLE_TYPE_DISCOVERY=on -DENABLE_TOPIC_DISCOVERY=off -DBUILD_TESTING=off -DBUILD_IDLC_XTESTS=off -DBUILD_EXAMPLES=off -DZPMALLOC=off -DWERROR=on -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .  -Bbuild
 wait
 cmake --build . --parallel
 wait
