41c7400bb31f9fe9cd7ad9bd62b39c6fb41962ca
assemblu <emirgoppa@gmail.com>
2023-11-07 16:32:21 +0100

    para update

--- cbuild.sh
+++ cbuild.sh
@@ -4,15 +4,17 @@ platform=$(uname)
 
 cd "`dirname $0`"
 echo "Navigating to: $PWD"
+rm -rf ../cyclonedds-certified/build/
 mkdir ../cyclonedds-certified/build/
 echo "Navigating to: $PWD"
 cd ../cyclonedds-certified/
-echo "Erasing previous build content"
-rm -rf *
-echo "Erasing previous install content"
-rm -rf ../install
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/emir/dev/cyclonedds-certified/install -DSANITIZER=none -DENABLE_SSL=off -DENABLE_SECURITY=off -DENABLE_LIFESPAN=off -DENABLE_DEADLINE_MISSED=on -DENABLE_TYPE_DISCOVERY=on -DENABLE_TOPIC_DISCOVERY=off -DENABLE_SHM=on -DBUILD_TESTING=off -DBUILD_IDLC_XTESTS=off -DBUILD_EXAMPLES=off -DZPMALLOC=off -DWERROR=on -DCMAKE_EXPORT_COMPILE_COMMANDS=1 . -Bbuild
-wait
-cpptesttrace --cpptesttraceOutputFile=/home/emir/dev/cyclonedds-certified/build/cyclonedds.bdf --cpptesttraceProjectName=cyclonedds cmake --build . --config Release --target install
-wait
+echi "-Bbuild\n"
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/emir/dev/cyclonedds-certified/install -DSANITIZER=none -DENABLE_SSL=off -DENABLE_SECURITY=off -DENABLE_LIFESPAN=off -DENABLE_DEADLINE_MISSED=on -DENABLE_TYPE_DISCOVERY=on -DENABLE_TOPIC_DISCOVERY=off -DENABLE_SHM=off -DBUILD_TESTING=on -DBUILD_IDLC_XTESTS=on -DBUILD_EXAMPLES=off -DZPMALLOC=off -DWERROR=on -DCMAKE_EXPORT_COMPILE_COMMANDS=1 . -Bbuild
+cd ./build/
+echo "buildRelease\n"
+cmake --build . --config Release --target install
+# echo "cpptesttrace\n"
+# cpptesttrace --cpptesttraceOutputFile=/home/emir/dev/cyclonedds-certified/build/cyclonedds.bdf --cpptesttraceProjectName=cyclonedds 
+echo "ctest\n"
+ctest --timeout 60 --no-compress-output --output-on-failure -T test -E '^CUnit_ddsrt_random_default_random|whc_check_end_state$' -C Release
 echo "[build script] finished"
